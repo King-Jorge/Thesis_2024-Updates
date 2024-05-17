50cc0063f511bfef4c77755784e8afc9551589fe
Amy Hurford <ahurford@mun.ca>
2024-05-17 14:31:18 -0230

    revised code

--- Amys codes/Travel_restrictions_isolation.R
+++ Amys codes/Travel_restrictions_isolation.R
@@ -1,32 +1,33 @@
 require(deSolve)
 # max daily rate of community member isolation
+u1max <-0.3
 u1max <- 1
 # max daily rate of traveller isolation
-u2max <- 5
+u2max <- 10
+
 # constraint on community member isolation
-C1max <- 1450
+C1max <- 1200
+C1max <- 200
 # constraint on traveller isolation
-C2max <- 500
+C2max <- 30
 # transmission rate
 beta<-.0002
-# importation rate (high for illustrative purposes)
-theta<-50
-theta<-1
+# importation rate
+theta<-2
 mu<-0.334
 # relative transmissibility of travellers
 c<-1
-T<-50
+T<-100
 
 # daily vaccination rate
 rho<-0
 
 # start time for community isolation
-tstart1<-2
+tstart1<-0
 
-# start time for travellers
-tstart2<-0
-
-toff1 <- 100
+# start time for restrictions on travellers
+tstart2<-10
+toff1 <- 1000
 ton <- toff1+5
 
 # community isolation function
@@ -52,6 +53,14 @@ u2fun<-function(t){
   return(u2)
 }
 
+# This is designed to trigger termination of the simulation when I1>0.5
+rootfun <- function(t, y, parameters){
+  I1 = y[2]
+  # Much higher Imin with importations
+  y1 = 0 - I1 
+  return(y1)
+}
+
 
 SI<-function(t, y, parameters){
   S = y[1]
@@ -62,32 +71,28 @@ SI<-function(t, y, parameters){
   u1 = u1fun(t)
   u2 = u2fun(t)
   
-  # Implements that resource constraints
-  if(C1>=C1max){
-    u1=0
+  if(C1 > C1max){
+    u1 = 0
   }
   
-  if(C2>C2max){
-    u2=0
+  if(C2 > C2max){
+    u2 = 0
   }
   
-  if(I1<=0.5){
-    I1=0
-  }
-  
-dS = - beta*S*(I1 + c*I2) - rho*S
-dI1 = beta*S*(I1 + c*I2) - mu*I1 - u1*I1
-dI2 = theta - 2*mu*I2 - u2*I2
-dC1 = u1*I1
-dC2 = u2*I2
+  dS = - beta*S*(I1 + c*I2) - rho*S
+  dI1 = beta*S*(I1 + c*I2) - mu*I1 - u1*I1
+  dI2 = theta - 2*mu*I2 - u2*I2
+  dC1 = u1*I1
+  dC2 = u2*I2
   return(list(c(dS,dI1,dI2,dC1,dC2)))
 }
 
-out <- ode(y = c(S=5000,I1=10,I2=1,C1=0,C2=0), parms = NULL, times = seq(0,T,.05), func = SI)
+out <- ode(y = c(S=5000,I1=10,I2=3,C1=0,C2=0), parms = NULL, times = seq(0,T,.05), func = SI, events = list(func = NULL, root = TRUE, terminalroot = 1), rootfun=rootfun)
 out <- data.frame(out)
+
 plot(out$time, out$I1, typ = "l")
-# Travellers are set to pretty high level to allow for visualization
-lines(out$time, out$I2, typ = "l", col="red")
+# Travellers are x50 to allow for visualization
+lines(out$time, 50*out$I2, typ = "l", col="red")
 # visualization to see that all resources are used up
 plot(out$time, out$C1, typ = "l")
 plot(out$time, out$C2, typ = "l")
